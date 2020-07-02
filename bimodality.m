88f31180a28cf583943c2e5938adc1c14480ae50
Soumyabrata <soumyabrata.dev@adaptcentre.ie>
2020-07-02 12:23:36 +0100

    m files added

---
+++ bimodality.m
@@ -0,0 +1,10 @@
+function pbi = bimod_degree_new(in)
+% This function calculates the Pearson's bimodal degree of a vector.
+% -----------------------------------------------------
+pbi = kurtosis(in, 1, 1) - power(skewness(in, 1, 1), 2);
+
+
+
+% This line is for calling bimod_degree_new function. 
+% Sir soumya can you please help me in describing what is J here. I think maybe it is our dataset
+bimod_degree_new(J);
